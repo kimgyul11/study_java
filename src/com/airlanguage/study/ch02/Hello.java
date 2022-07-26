c9cb779470825a1d53bdc68a9bb402ba1cc48bc8
kimgyul11 <coloo88@naver.com>
2022-07-26 16:38:29 +0900

    c

---
+++ src/com/airlanguage/study/ch02/Hello.java
@@ -0,0 +1,42 @@
+package com.airlanguage.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		boolean abc = true;
+		boolean xyz;
+		
+		
+		String nationality = "한국";
+		String yourhome = "한국";
+		String yourlanguage = "한국어";
+		String Available_Languages = "한국어 일본어";
+		int select = 11;
+		int like = 4;
+		String level = "초심자";
+		int year = 1992;
+		int month = 10;
+		int day = 13;
+		String phone = "010-5557-6295";
+		String id = "rldbfcjswo";
+		String nickname = "KIMGIYUL";
+		String email = "coloo88@naver.com";
+		
+		String name = "김기율";
+		System.out.println("안녕하세요 저는"+name+"이라고합니다");
+		System.out.println("당신의 국적은:"+ nationality);
+		System.out.println("거주하는 곳은:" + yourhome);
+		System.out.println("당신의 언어는:" + yourlanguage);
+		System.out.println("좋아요 받은수:"+select);
+		System.out.println("태어난 년도:"+year);
+		System.out.println("태어난 달:"+month);
+		System.out.println("태어난 일:"+day);
+		System.out.println("핸드폰 번호는:"+phone);
+		System.out.println("아이디 :"+ id);
+		System.out.println("닉네임 :"+nickname);
+		System.out.println("이메일 :"+email);
+		
+		
+	}
+
+}
