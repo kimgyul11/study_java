d245a3e981e5dc3b1ef0eb42e4744fa274a1ae81
kimgyul11 <coloo88@naver.com>
2022-07-29 15:58:20 +0900

    c

---
+++ src/com/airlanguage/study/ch02/IntExample.java
@@ -0,0 +1,16 @@
+package com.airlanguage.study.ch02;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		
+		long var1 = 10;
+		long var2 = 20;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		
+	}
+
+}
