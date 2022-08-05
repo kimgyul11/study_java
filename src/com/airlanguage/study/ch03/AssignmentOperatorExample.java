e17cf2bb1f9215a80f92c24c58b7bd950e28a266
kimgyul11 <coloo88@naver.com>
2022-08-05 11:46:46 +0900

    commit

---
+++ src/com/airlanguage/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,25 @@
+package com.airlanguage.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+
+//		+=
+		
+		int result = 0;
+		int result2 = 0;
+		int result3 = 0;
+		
+		result += 10;
+		//result = result + 10;
+		System.out.println("result = "+result);
+	
+		result2 -= 10;
+		System.out.println("result = "+result2);
+		
+		result3 *= 10;
+		System.out.println("result = " +result3);
+	
+	
+	}
+}
