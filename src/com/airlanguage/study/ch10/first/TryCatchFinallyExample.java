0ab79f47f16b0dd550615d55d00d78dc8253c2f8
kimgyul11 <coloo88@naver.com>
2022-09-22 17:56:19 +0900

    java-Exception

---
+++ src/com/airlanguage/study/ch10/first/TryCatchFinallyExample.java
@@ -0,0 +1,16 @@
+package com.airlanguage.study.ch10.first;
+
+public class TryCatchFinallyExample {
+
+	public static void main(String[] args) {
+		
+//		try {
+//			Class clazz = Class.forName("java.lang.String2");
+//		} catch(ClassNotFoundException e) {
+//			System.out.println("클래스가 존재하지 않습니다.");
+//		}
+	}
+
+	}
+
+
